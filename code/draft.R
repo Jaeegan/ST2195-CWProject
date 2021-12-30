934c5f3db95278262a1adc5a1dcfa750c86b336d
Joshua <jgan003@mymail.sim.edu.sg>
2021-12-31 03:30:17 +0800

    Update

--- code/draft.R
+++ code/draft.R
@@ -14,6 +14,7 @@ setwd("~/../Joshua PFDS/r/coursework/data")
 # load data
 plane_data <- read.csv("plane-data.csv", header = TRUE)
 carriers <- read.csv("carriers.csv", header = TRUE)
+airports <- read.csv("airports.csv", header = TRUE)
 fd_1995 <- read.csv("1995.csv", header = TRUE)
 fd_1996 <- read.csv("1996.csv", header = TRUE)
 #fd_1997 <- read.csv("1997.csv", header = TRUE)
@@ -61,7 +62,9 @@ fd_raw %>%
 # since our interest is in the delays of flights, we shall remove incomplete observations
 # and create a new data frame with only complete observations.
 fd_comp <- fd_raw %>% 
-  filter(!is.na(DepDelay), !is.na(ArrDelay))
+  filter(!is.na(DepDelay), !is.na(ArrDelay)) %>% 
+  select(-TaxiIn, -TaxiOut, -CancellationCode, -CarrierDelay, 
+         -WeatherDelay, -NASDelay, -SecurityDelay, -LateAircraftDelay)
 
 
 # Intuitively, any missing value (NA) in ArrDelay would be a result of Cancelled or 
@@ -111,101 +114,106 @@ plane_data$year <- plane_data$year %>% as.numeric()
 
 # Create a table with plane manufactured year identified by tail number
 plane_reldate <- plane_data %>% 
-  select(TailNum = tailnum, 
-         Release = year)
+  select(TailNum = tailnum, year)
 
-  
-# Create a table with carrier name and plane manufactured year merged by tail number
-# carrier_tailnum <- merge(fd_comp, carriers, by.x = "UniqueCarrier", 
-#                          by.y = "Code", all.x = TRUE, all.y = FALSE) %>% 
-#   select(Carrier = Description, TailNum) %>% 
-#   distinct()
-
-# plane_reldate <- merge(carrier_tailnum, plane_reldate, by = "TailNum") %>% 
-#   filter(Release <= 2000)
-
-
+# We create a joined delay indicator. if the dep/ arr delay is more than 15 minutes as 1,
+# 0 otherwise.
 fd_new <- fd_comp %>% 
-  mutate(DepDel0 = ifelse(DepDelay < 15, 0, DepDelay),
-         DepDel15 = ifelse(DepDelay >= 15, 1, 0),
-         ArrDel0 = ifelse(ArrDelay < 15, 0, ArrDelay),
-         ArrDel15 = ifelse(ArrDelay >= 15, 1, 0))
+  mutate(DepDel15 = ifelse(DepDelay >= 15, 1, 0),
+         ArrDel15 = ifelse(ArrDelay >= 15, 1, 0),
+         Delayed = ifelse(DepDel15 == 1 | ArrDel15 == 1, 1, 0)) %>% 
+  select(-Cancelled, -Diverted)
 
-fd_reldate <- merge(fd_new, plane_reldate, by = "TailNum") %>%
-  mutate(Delayed = ifelse(DepDel15 == 1 | ArrDel15 == 1, 1, 0)) %>% 
-  select(TailNum, UniqueCarrier, Delayed, Release) %>% 
-  group_by(TailNum, UniqueCarrier, Release) %>% 
-  summarise(Delayed_Count = sum(Delayed))
-  
+fd_reldate <- merge(fd_new, plane_reldate, by = "TailNum", all.x = TRUE, all.y = FALSE) %>%
+  group_by(TailNum, UniqueCarrier, year) %>% 
+  summarise(total_delays = sum(Delayed))
 
-plcr_reldate <- merge(fd_reldate, carriers, by.x = "UniqueCarrier", by.y = "Code",
-                       all.x = TRUE, all.y = FALSE)
-  
+# Check for possible duplication of Tail numbers  
+fd_reldate %>% 
+  group_by(TailNum) %>% 
+  summarise(Count = n()) %>% 
+  arrange(desc(Count))
 
-# plcr_reldate %>% count(length(unique(TailNum)))
+# After reviewing the results, there are records with duplicate Tail numbers.
+# This could suggest possible entry errors in the original data.
 
-# From plane data, we calculate the average manufacturing year as a reference, where
+# Check for missing data
+summary(fd_reldate)
+
+# We have records on an estimated 3634 planes (by tail number).
+# Unfortunately, we do not have the manufactured data for 2431 of those planes.
+# Since we are interested in the age of the planes on delay, the incommplete records
+# will be removed.
+fd_reldate <- fd_reldate %>% 
+  filter(!is.na(year))
+
+
+fd_reldate <- merge(fd_reldate, carriers, by.x = "UniqueCarrier", by.y = "Code",
+                       all.x = TRUE, all.y = FALSE) %>% 
+  select(TailNum, UniqueCarrier, carrier = Description, everything()) %>% 
+  rename_all(tolower)
+
+# From the plane data, we calculate the average manufacturing year as a reference, where;
 # before average = older planes,
 # after average = newer planes.
 plane_data %>%
   filter(!is.na(year), year !=  0) %>% 
   select(year) %>%
   arrange(year) %>% 
-  summarise(AvgYear = round(mean(year), 0))
+  summarise(avg_year = round(mean(year), 0))
 
-# For each year, we calculate the number of plane delays and 'age group' of the plane
-test <- plcr_reldate %>% 
-  group_by(Release) %>% 
-  summarise(Count = sum(Delayed_Count)) %>%
-  filter(!is.na(Release)) %>% 
-  arrange(Release) %>% 
-  mutate(Age = ifelse(Release >= 1997, "new", "old"))
+# For each year, we calculate the number of plane delays and create an 'age group' indicator
+test <- fd_reldate %>% 
+  group_by(year) %>% 
+  summarise(total_delays = sum(total_delays)) %>% 
+  mutate(age = ifelse(year >= 1997, "new", "old")) 
 
-# Conduct a hypothesis test to compare equality of the two variance
-res.ftest <- var.test(Count ~ Age, data = test)
+# We conduct a hypothesis test to compare equality of the two variance
+res.ftest <- var.test(total_delays ~ age, data = test)
 
 res.ftest
 # As the p-value = 6.468e-8%, which is less than the 1% significance level.
 # There is strong evidence to reject the null hypothesis and conclude that the true
-# variance are not equal
+# variances are not equal
 
 # Conduct a unpaired left-tailed t-test
-# Null hypothesis: average new planes delay - average old plane delay >= 0
-# Alt hypothesis: average new planes delay - average old plane delay < 0
-res <- t.test(Count ~ Age, data = test, var.equal = FALSE, alternative = "less")
+# Null hypothesis: average new planes delay - average old planes delay >= 0
+# Alt hypothesis: average new planes delay - average old planes delay < 0
+res <- t.test(total_delays ~ age, data = test, var.equal = FALSE, alternative = "less")
 
 res
 # As the p-value = 0.01266%, which is less than the 1% significance level.
 # There is strong evidence to reject the null hypothesis and conclude that older planes
 # do suffer from more delays
-
-
------------------------------------------------------------------------------------------------
   
-bx_data <- plcr_reldate %>% 
-  filter(!is.na(Release)) %>% 
-  group_by(Release, UniqueCarrier) %>% 
-  summarise(Count = sum(Delayed_Count)) %>%
-  arrange(Release) %>% 
-  mutate(Age = ifelse(Release >= 1997, "new", "old"))
+bx_data <- fd_reldate %>% 
+  group_by(year, uniquecarrier) %>% 
+  summarise(total_delays = sum(total_delays)) %>%
+  mutate(age = ifelse(year >= 1997, "new", "old"))
  
 bx_data %>% 
-  ggplot(aes(UniqueCarrier, Release)) +
-  geom_boxplot(aes(fill = UniqueCarrier),
-            alpha = 0.5, outlier.size = 3, outlier.shape = 4, outlier.stroke = 2) +
-  geom_hline(yintercept = 1997, color = "red", linetype = "dashed", size = 1) + 
+  ggplot(aes(year, total_delays, fill = uniquecarrier)) +
+  geom_bar(stat = "identity") +
+  geom_vline(xintercept = 1997, color = "red", linetype = "dashed", size = 0.3) +
+  geom_text(aes(x=1997, label="\nnew planes", y=35000), colour="#69b3a2", angle=90, size = 3.2) +
+  geom_text(aes(x=1997, label="old planes\n", y=35000), colour="#CC6666", angle=90, size = 3.2) +
+  guides(fill=guide_legend(title="Unique Carrier")) +
+  facet_wrap(~uniquecarrier, ncol = 3) +
   labs(
-    y = "Year",
-    title = "Boxplot of individual carrier delays by manufactured year"
+    title = "Multiple Bar Charts - Total Delays by Manufactured Year by Carrier",
+    x = "Manufactured Year",
+    y = "Delay Count"
   )
 
-# The average planes delay by manufactured year for each carrier is below the "1997" reference
-# mark. Hence, we can conclude majority of flight delays are caused by older planes which make
-# sense given the nature of the data we used for analysis has more than 50% of the flights
-# made before 1997. This result is further supported by the hypothesis test.
+# The majority of planes delay by manufactured year for each carrier is below the "1997" reference
+# mark. However, this graphical representation is not sufficient enough to conclude older planes
+# do indeed result in more delays, and should be interpreted together with a hypothesis test result.
 
-# We can observe 2 outliers from the plot specifically from US and CO.
+# There seems to be a pattern in plane delays by manufactured year. More specifically, planes 
+# that were manufactured in the year 1989-1991 appears to have contributed to the majority of 
+# delays.
 
+------------------------------------------------------------------------------------------------
 
 # For each carrier, we calculate the average delays (in minutes)
 fd_new %>% 
@@ -216,16 +224,229 @@ fd_new %>%
     ) %>% 
   arrange(desc(AvgDepDelay))
   
-
+# Percentage of delays within each carrier
 fd_new %>% 
+  group_by(UniqueCarrier) %>% 
+  select(UniqueCarrier, Delayed) %>% 
+  summarise(
+    Total_Delays = sum(Delayed),
+    PerDelays = mean(Delayed))
+
+# Percentage of delays between the carrier
+donut <- fd_new %>% 
   group_by(UniqueCarrier) %>% 
   summarise(
     TotalFlight = n(),
-    PerDepDelay = mean(DepDelay >= 15)) %>% 
-  arrange(desc(PerDepDelay))
+    n = sum(Delayed)) %>% 
+  ungroup() %>% 
+  summarise(
+    UniqueCarrier,
+    PerDelays = n/ sum(n)
+  ) %>% 
+  arrange(PerDelays)
 
-  
+
+donut$ymax = cumsum(donut$PerDelays)
+donut$ymin = c(0, head(donut$ymax, n=-1))
+donut$labelPosition <- (donut$ymax + donut$ymin)/ 2
+donut$label <- paste0(donut$UniqueCarrier, "\n", round(donut$PerDelays, 4)*100, " %")
+
+
+donut %>% 
+  ggplot(aes(ymax=ymax, ymin=ymin, xmax=4, xmin=3, fill=UniqueCarrier)) +
+  geom_rect() +
+  geom_text(x = 2.5, aes(y = labelPosition, label = label, color = UniqueCarrier), size = 3) +
+  scale_fill_brewer(palette = "BrBG") +
+  scale_color_brewer(palette = "BrBG") +
+  coord_polar(theta="y") +
+  xlim(c(0, 5)) +
+  theme_void() +
+  labs(
+    title = "Donut Plot - Percentage of Delays by Carrier"
+  )
+ 
 -----------------------------------------------------------------------------------------------
+
+# How does the number of people flying between different locations change over time?
+# Lets check the structure of the data before we begin  
+glimpse(fd_new) 
+   
+fd_new$route <- with(fd_new, paste(Origin, Dest, sep = "-"))  
+  
+fd_routes <- fd_new %>% 
+  select(-contains("Time"), -contains("Del"), -contains("Num"), -Distance)
+  
+head(fd_routes)
+  
+# Check for number of unique routes
+fd_routes %>% 
+  group_by(route) %>% 
+  summarise(count = n()) %>% 
+  arrange(desc(count))
+
+
+# For each month, we calculate the change in flights by carrier
+fd_routes %>% 
+  group_by(Year, Month, route, UniqueCarrier) %>% 
+  summarise(count = n())
+  
+monthly_rep <- fd_routes %>% 
+  group_by(Month, route) %>% 
+  summarise(count = n()) %>% 
+  arrange(route) %>% 
+  group_by(route) %>% 
+  mutate(
+    MoM = round((count - lag(count, n = 1, default = NA))*100/ lag(count, n = 1, default = NA), 2)
+  ) 
+
+route_count <- fd_routes %>% 
+  group_by(Month, route) %>% 
+  summarise(count = n()) %>% 
+  spread(key = route, value = count)
+
+perlag <- monthly_rep %>%
+  mutate(Month  = factor(Month, levels = c(1:12))) %>% 
+  select(Month, route, MoM) %>% 
+  spread(key = route, value = MoM)
+
+
+report <- mapply(rbind, route_count, perlag) 
+
+report <- t(report[,-1])
+
+col_lab1 <- as.data.frame(month.abb)
+col_lab2 <- rep("MoM (%)", 12) %>% 
+  as.data.frame()
+
+report_label <- mapply(rbind, col_lab1, col_lab2)
+
+colnames(report) <- as.character(report_label)
+
+
+----------------------------------------------------------------------------
+
+# Check popularity of destination by number of flight arrivals by month
+fd_routes %>% 
+  group_by(Dest, Month) %>% 
+  summarise(count = n()) %>% 
+  arrange(desc(count))
+
+# Multiple Barplot for number of flights by season for each unique destinations
+fd_routes %>% 
+  group_by(Month, Dest) %>% 
+  summarise(count = n()) %>%
+  mutate(Month = month.abb[Month] %>% factor(levels = month.abb)) %>% 
+  ggplot(aes(x = Month, y = count)) +
+  geom_bar(stat = "identity", fill = "pink") +
+  facet_wrap(~Dest, ncol = 13) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+# Multiple Barplot for number of flights by season for each outgoing flight from ORD (Chicago)
+fd_routes %>% 
+  filter(Origin == "ORD") %>% 
+  group_by(Month, route) %>% 
+  summarise(count = n()) %>%
+  mutate(Month = month.abb[Month] %>% factor(levels = month.abb)) %>% 
+  ggplot(aes(x = Month, y = count)) +
+  geom_bar(stat = "identity", fill = "lightblue") +
+  facet_wrap(~route, ncol = 13) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+
+# Multiple line chart for number of flights by time for each unique destination 
+# Removed records with less than 100 flights to destination
+fd_routes %>% 
+  mutate(date = with(fd_routes, paste(Year, Month, sep = "-")) %>% as.yearmon()) %>% 
+  group_by(date, Dest) %>% 
+  summarise(count = n()) %>%
+  filter(count > 100) %>% 
+  ggplot(aes(x = date, y = count)) +
+  geom_line() +
+  facet_wrap(~Dest, ncol = 20) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+
+
+library(zoo)
+
+# We sample 1 airport, we check the number of flights by routes by month
+# Lets check which airport has the most number of outgoing flights in a year
+fd_routes %>%
+  group_by(Year, Origin) %>% 
+  summarise(count = n()) %>%
+  arrange(desc(count))
+
+# ORD - Chicago O'Hare International has the most number of outgoing flights yearly.
+# We shall use fight data from the ORD airport as our sample.
+
+fd_routes %>% 
+  mutate(date = with(fd_routes, paste(Year, Month, sep = "-")) %>% as.yearmon()) %>% 
+  filter(Origin == "ORD") %>% 
+  group_by(date, route) %>%
+  summarise(count = n()) %>% 
+  ggplot(aes(x = date, y = count)) +
+  geom_line() +
+  facet_wrap(~route, ncol = 13) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+# From the graphs, we are able to observe the trend of outgoing flights from ORD (Chicago) 
+# over the years from 1995 to 1996 (left to right). Majority of the routes showed consistent
+# outgoing flights with a few notable ones, namely "ORD-BOS", "ORD-FLL", "ORD-LAX", "ORD-SEA".
+# Flights from Chicago to Los Angeles has picked up from Mar 1995 and remained consistently high.
+
+------------------------------------------------------------------------------------------------
+
+  # ord_rep <- fd_routes %>% 
+#   filter(Origin == "ORD") 
+# 
+# ord_rep$date <- with(ord_rep, paste(Year, Month, sep = "-")) %>% 
+#   as.yearmon()
+# 
+# 
+# ord_rep <- ord_rep %>% 
+#   group_by(date, route) %>%
+#   summarise(count = n()) %>% 
+#   arrange(route) %>% 
+#   group_by(route) %>% 
+#   mutate(
+#     MoM = round((count - lag(count, n = 1, default = NA))*100/ lag(count, n = 1, default = NA), 2)
+#   ) 
+#   
+# ord_rep %>% 
+#   ggplot(aes(x = date, y = count)) +
+#   geom_line() +
+#   facet_wrap(~route, ncol = 13) +
+#   theme_minimal() +
+#   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+
+
+count, location, time
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 depdel_tm <- flight_data_raw %>% 
   filter(DepDelay >= 15, na.rm = TRUE) %>% 
